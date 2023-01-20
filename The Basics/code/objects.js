1d42a3c1dc01b254567d5fe0dc3b747114c34b51
Matheus Santos Ribeiro <matheussantos@Matheuss-MacBook-Air.local>
2023-01-20 20:26:21 -0300

    Added more info to objects.js

--- The Basics/code/objects.js
+++ The Basics/code/objects.js
@@ -2,7 +2,11 @@ const user = {
     name: "Matheus",
     age: 24,
     married: false,
-
+    products: ["Cell", "Computer", "Videogame"]
 }
 
 user.age; // Access the age inside the user object
+
+//To access a value inside an array in a object use:
+
+user.products[0]; // This example access the first value of the array witch is "Cell" (I discoverd this by my self and I am very proud :) )
