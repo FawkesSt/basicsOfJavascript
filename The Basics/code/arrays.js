735b38e95536c29604dec00501f31bd17f55a4ca
Matheus Santos Ribeiro <matheussantos@Matheuss-MacBook-Air.local>
2023-01-20 21:14:47 -0300

    Added array file

---
+++ The Basics/code/arrays.js
@@ -0,0 +1,14 @@
+const users = ["Matheus", "Lê", "Joe", "Jammes"];
+
+console.log(users);
+
+// to access a value inside an array we can use:
+users[0]; // We pass the position of the value that we want, for example 0. This will return the first value of the array.
+
+console.log(users[1] + " I lov u");
+
+// Array is a object as well, so we can use some methods available.
+
+console.log(users.length); // This method for example brings us the size of the array
+
+console.log(users.splice(4,4));
